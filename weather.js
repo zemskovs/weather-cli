2c113c3215ce9129f4b1ca41ede8387976c0c659
zemskov.va <vladimir.zemskov@dualbootpartners.com>
2021-12-16 08:53:26 +0300

    add print Weather

--- weather.js
+++ weather.js
@@ -1,19 +1,68 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { printError, printHelp, printWeather } from './services/log.service.js';
+import {
+  saveKeyValue,
+  TOKEN_DICTIONARY,
+  getKeyValue,
+} from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+
+const saveCity = async (city) => {
+  if (!city.length) {
+    printError('Не передан город');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.city, city);
+    printSuccess('Город сохранен');
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError('Не передан токен');
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess('Токен сохранен');
+  } catch (error) {
+    printError(error.message);
+  }
+};
+
+const getForecast = async () => {
+  try {
+    const city = process.env.CITY ?? (await getKeyValue(TOKEN_DICTIONARY.city));
+    const weather = await getWeather(city);
+    printWeather(weather, getIcon(weather.weather[0].icon));
+  } catch (e) {
+    if (e?.response?.status == 404) {
+      printError('Неверно указан город');
+    } else if (e?.response?.status == 401) {
+      printError('Неверно указан токен');
+    } else {
+      printError(e.message);
+    }
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
   console.log(args);
   if (args.h) {
-    // Вывод help
+    printHelp();
   }
   if (args.s) {
-    // Сохранить город
+    saveCity(args.s);
   }
   if (args.t) {
-    // Сохранить токен
+    saveToken(args.t);
   }
-  // Вывести погоду
+  getForecast();
 };
 
 initCLI();
